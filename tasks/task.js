5f68009b3adfd62ae49e4d57466d564408048ad7
Jan Kockrow <jan.kockrow@auctionata.com>
2015-02-16 09:15:18 +0100

    first commit

---
+++ tasks/task.js
@@ -0,0 +1,22 @@
+var shell = require('shelljs');
+
+//.\\bin\\stylecop\\StyleCop.Console.exe
+
+module.exports = function (grunt) {
+
+  function run_stylecop(options) {
+    if(shell.exec(options.path + ' -p "'+ options.include +'"') !== 0) {
+      grunt.fail.warn('');
+    };  
+  }
+
+  grunt.registerTask('stylecop', function() {
+    run_stylecop('..\\server');
+  });
+
+  grunt.registerMultiTask('stylecop', 'Runs static code analysis with StyleCop.', function() {
+    var options = this.options({});
+    run_stylecop(options);
+  });
+
+};
